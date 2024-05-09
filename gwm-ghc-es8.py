2cf0f91ce022ce4e1f65bf19fa2cdf905bc18e6e
Yongguang Zhu <heavenwing@msn.com>
2024-05-09 06:48:57 +0000

    update script using diff index

--- gwm-ghc-es8.py
+++ gwm-ghc-es8.py
@@ -4,7 +4,7 @@
 # the original script also contains whitelist of requests and basic authentication.
 
 import asyncio
-from mitmproxy import http,ctx
+from mitmproxy import http, ctx
 from elasticsearch import Elasticsearch, ConnectionTimeout, ApiError
 from datetime import datetime
 import base64
@@ -34,6 +34,7 @@ es = Elasticsearch(
 
 
 class SaveLogtoElasticSearch:
+
     def __init__(self):
         self.loop = asyncio.get_event_loop()
         self.proxy_authorizations = {} 
@@ -54,7 +55,6 @@ class SaveLogtoElasticSearch:
         # get proxy-authorization http://username@proxy_address:port
         proxy_auth = flow.request.headers.get("Proxy-Authorization", "")
 
-        
         # verify username whether exist and be in user_list
         if not proxy_auth:
             flow.response = http.Response.make(401)
@@ -70,20 +70,26 @@ class SaveLogtoElasticSearch:
             return
         
         ctx.log.info("Authenticated User: " + username + "@" + flow.client_conn.address[0])
+        # flow.metadata["username"] = username
         self.proxy_authorizations[(flow.client_conn.address[0])] = username
 
-
     def response(self, flow: http.HTTPFlow):
-        # async save request and response to elasticsearch
-        asyncio.ensure_future(self.save_to_elasticsearch(flow))
-
-    async def save_to_elasticsearch(self, flow: http.HTTPFlow):
         # only capture requests which contain "completions" and "telemetry"
         if flow.request.url.find("completions") == -1 and flow.request.url.find('telemetry') == -1:
             return
 
-        username = self.proxy_authorizations.get(flow.client_conn.address[0])
+        # async save request and response to elasticsearch
+        asyncio.ensure_future(self.save_to_elasticsearch(flow))
 
+    async def save_to_elasticsearch(self, flow: http.HTTPFlow):
+        username = self.proxy_authorizations.get(flow.client_conn.address[0])
+        # username = flow.metadata.get("username")
+        if (username is None):
+            ctx.log.warn(f"Cannot get username from metadata")
+            return
+        url_type = flow.request.url.split('/')[-1]
+        ctx.log.info(f"Saving request and response to elasticsearch for user: {username}, url_type: {url_type}")
+        
         # Add "ms" to the end of the timeconsumed string
         timeconsumed = round((flow.response.timestamp_end - flow.request.timestamp_start) * 1000, 2)
         timeconsumed_str = f"{timeconsumed}ms"  
@@ -94,8 +100,8 @@ class SaveLogtoElasticSearch:
             "timestamp": datetime.utcnow().isoformat(),
             "proxy-time-consumed": timeconsumed_str,  # Use the modified timeconsumed string
             'request': {
-                'url': flow.request.url,
-                'method': flow.request.method,
+                # 'url': url_type,
+                # 'method': flow.request.method,
                 'headers': dict(flow.request.headers),
                 'content': flow.request.content.decode('utf-8', 'ignore'),
             },
@@ -107,8 +113,15 @@ class SaveLogtoElasticSearch:
         }
         
         try:
-            #change the index name to your own
-            index_func = functools.partial(es.index, index='mitmproxy', body=doc)
+            # change the index name to your own
+            if url_type == 'completions':
+                index_name = 'github-copilot-completions'
+            if url_type == 'telemetry':
+                index_name = 'github-copilot-telemetry'
+            if index_name is None:
+                ctx.log.warn(f"Don't support this url type: {url_type}")
+                return
+            index_func = functools.partial(es.index, index=index_name, body=doc)
             await self.loop.run_in_executor(None, index_func)   
         except ConnectionTimeout as e:
             ctx.log.error(f"Connection to Elasticsearch timed out: {e}") 
